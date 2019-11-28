05f8e06d5cda760a411053b057fbaf00ca467704
JesusDD <35343377+JesusDD@users.noreply.github.com>
2019-11-28 15:54:50 -0600

    Update TheArchitect

--- mazegamesource/TheArchitect.java
+++ mazegamesource/TheArchitect.java
@@ -1,33 +1,26 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.io.File;
 import javax.swing.*;
-//Your life is the sum of a remainder of an unbalanced equation inherent to the programming
-//of the matrix
 
-public class TheArchitect extends JFrame
-{
-   public void setExit(int x, int y)//records the location of the exit so we can show it when its time
-   {
+public class TheArchitect extends JFrame {
+	
+   public void setExit(int x, int y){
        WallXCord=x;
        WallYCord=y;  
    } 
-   public void showWall()//used when its time to show the exit.  
-   {
+   public void showWall(){
        updatedMatrix[WallXCord][WallYCord]="E";  
    }
 
-    public void playerMove(int xScale, int yScale, String[][] currentMatrix,int totalDimonds)throws StupidAssMove
+    public void playerMove(int xScale, int yScale, String[][] currentMatrix,int totalDimonds)throws MovimientoNoValido
     {
     	int [] playerPos = {0,0};
-    	globalTotalDimonds=totalDimonds; //use this later for the gui dimond count
-    	nextLevel(false); //dont go to the next level yet.
+    	globalTotalDimonds = totalDimonds; 
+    	nextLevel(false); 
         
     	playerPos = locatePlayerPos(currentMatrix);
         
         if(itsADimond(currentMatrix, playerPos, xScale, yScale)){
         	changeValues(currentMatrix, playerPos, xScale, yScale);
-            collected+=1;//we got a dimond!
+            collected+=1;
         } else if(itsAMovableWall(currentMatrix, playerPos, xScale, yScale)){
         	changeValues(currentMatrix, playerPos, xScale, yScale);
             currentMatrix[playerPos[0]+(xScale*2)][playerPos[1]+(yScale*2)]="M";
@@ -35,62 +28,59 @@ public class TheArchitect extends JFrame
         	changeValues(currentMatrix, playerPos, xScale, yScale);
         } else if (itsAExit(currentMatrix, playerPos, xScale, yScale)){
         	changeValues(currentMatrix, playerPos, xScale, yScale);
-            nextLevel(true);//allow the next level to be loaded.
+            nextLevel(true);
         } else{
-            throw new StupidAssMove("Ass Hole hit wall!");
+            throw new MovimientoNoValido("Ass Hole hit wall!");
         }
-        
-        if(collected==totalDimonds){ //if we have all the dimonds give the player the exit
+      //Si el jugador recogio todos los diamantes se le muestra la salida
+        if(collected==totalDimonds){ 
             showWall();
         }
                
-        updatedMatrix=currentMatrix;  //we will return updatedMatrix for the gui                     
-    }//end method
+        updatedMatrix = currentMatrix;                       
+    }
 
-    public void nextLevel(boolean tOrF)//true we go to next level, false we update current level's gui 
-    {
-        level=tOrF;
+	public void nextLevel(boolean tOrF) {
+    	level=tOrF;
     }
     
-    public boolean getLevel()//returs level true or false
-    {
+    public boolean getLevel() {
         return level;
     }
         
-    public int getDimondsLeft()
-    {
-        return globalTotalDimonds-collected;//for GUI JLabel, show how many dimonds are left to be collected
+    public int getDimondsLeft() {
+        return globalTotalDimonds - collected;
     }
     
-    public String[][] getUpdatedMatrix()//returns the updated matrix for the gui to display
-    {
+    public String[][] getUpdatedMatrix(){
         return updatedMatrix;    
     }
     
     private int [] locatePlayerPos(String[][] currentMatrix) {
     	int [] pos = {0,0};
-    	for (int i = 0; i < currentMatrix.length; i++){ //for loop will find were the player is now
+    	for (int i = 0; i < currentMatrix.length; i++){ 
             for (int j = 0; j < currentMatrix[i].length; j++){
-                if(currentMatrix[i][j].equals("P")){//we found the player
-                	//record the players position
+                if(currentMatrix[i][j].equals("P")){
                     pos[0]=i;
                     pos[1]=j;
                     break;
                 }
             }
-        }//end both for loops
+        }
     	return pos;
     }
+    
     private String [][] changeValues(String[][] currentMatrix, int [] pos, int xScale, int yScale){
     	String [][] matrix = currentMatrix;
     	matrix[pos[0]][pos[1]]="N";
         matrix[pos[0]+xScale][pos[1]+yScale]="P";
         return matrix;
     }
+    
     private boolean itsADimond(String[][] currentMatrix, int [] pos, int xScale, int yScale){
         return currentMatrix[pos[0]+xScale][pos[1]+yScale].equals("H")||currentMatrix[pos[0]+xScale][pos[1]+yScale].equals("D");
     }
-    
+        
     private boolean itsAMovableWall(String[][] currentMatrix, int [] pos, int xScale, int yScale){
         return currentMatrix[pos[0]+xScale][pos[1]+yScale].equals("M") && currentMatrix[pos[0]+(xScale*2)][pos[1]+(yScale*2)].equals("N");
     }
@@ -103,20 +93,18 @@ public class TheArchitect extends JFrame
         return currentMatrix[pos[0]+xScale][pos[1]+yScale].equals("E");
     }
     
-    private class StupidAssMove extends RuntimeException
-    {
-         public StupidAssMove(String event)
-         {
+    private class MovimientoNoValido extends RuntimeException{
+         public MovimientoNoValido(String event){
              JFrame frame = new JFrame("Warning");
-             JOptionPane.showMessageDialog(frame, "You Stupid Ass, Ran into something did you?");
+             JOptionPane.showMessageDialog(frame, "Chocaste contra una pared");
          }
-    }//end inner class
+    }
     
-int foundPlayer=0;
+//int foundPlayer=0;
 String[][] updatedMatrix;
 int WallXCord;
 int WallYCord;
 int collected=0;
 boolean level;
 int globalTotalDimonds=0;
-}//end class
+}
