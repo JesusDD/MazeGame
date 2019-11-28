1461887412c1c11f7d99441399c0cd81dc7dd6a0
JesusDD <35343377+JesusDD@users.noreply.github.com>
2019-11-28 15:52:44 -0600

    Update TimeCalculator

--- mazegamesource/TimeCalculator.java
+++ mazegamesource/TimeCalculator.java
@@ -1,33 +1,31 @@
-public class TimeCalculator
-{
+public class TimeCalculator {
 int minutes=0;
 int seconds=0;
 
-    public void calcTimeforMaze(int totalDimonds, int xSize, int ySize)//some kinda method that determines the time a player has for each level based on the level size and dimonds.
-    {
-        if(xSize/ySize < 1)//this method should be changed in order to provide a more relistic time system.
-        {
-            minutes+=(ySize/xSize)*1+1;
+	public void calcTimeforMaze(int totalDimonds, int xSize, int ySize){
+		if(xSize/ySize < 1){
+			minutes+=(ySize/xSize)*1+1;
+        } else{
+        	minutes+=(ySize/xSize)*1+1;
         }
-        else
-            minutes+=(ySize/xSize)*1+1;
-        if(totalDimonds >6 && totalDimonds*.10 + seconds <= 60)
-            minutes+=(ySize/xSize)*1+1;
-        else
-        {
+		
+		if(totalDimonds >6 && totalDimonds*.10 + seconds <= 60) {
+			minutes+=(ySize/xSize)*1+1;
+		} else {
             minutes+=1;          
         }
-        if(minutes ==0)
-           minutes=2;
-     }//end method
+		
+        if(minutes ==0) {
+        	minutes=2;
+        }
+     }
     
-    public int getMinutes()
-    {
+    public int getMinutes() {
         return minutes;    
     }
 
-    public int getSeconds()
-    {
+    public int getSeconds() {
         return seconds;
     }
-}//end class
+    
+}
