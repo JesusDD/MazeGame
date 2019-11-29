3644939de372ec1c8562a4eb9c2d3271cc898573
JesusDD <35343377+JesusDD@users.noreply.github.com>
2019-11-28 18:56:17 -0600

    Update FileLoader.java

--- mazegamesource/FileLoader.java
+++ mazegamesource/FileLoader.java
@@ -5,8 +5,8 @@ public class FileLoader{
 	private int exitXCord=0;
 	private int exitYCord=0;;
 	private String[][] GameMatrix;
-	private int column;
-	private int row;
+	private int columna;
+	private int fila;
 	private BufferedReader in;
 	
 	public void loadFile(String fileName){  
@@ -27,36 +27,35 @@ public class FileLoader{
 	public void MatrixLoader(String fileTextLine, int lineNum)throws gameFileError{
         int sum=0;
         char textVar;
-      //se checa si estamos en la primera linea del maze, de ser asi se crea la Matriz con base a esa linea
+        //se checa si estamos en la primera linea del maze, de ser asi se crea la Matriz con base a esa linea
         if(lineNum == 0){
-        	for(int i=0; i<fileTextLine.length();i++){
-        		if(fileTextLine.charAt(i) ==' ') {//find blank area on first line number 
-        			sum+=1;
+        	for(int i = 0; i < fileTextLine.length();i++){
+        		if(fileTextLine.charAt(i) == ' ') { 
+        			sum += 1;
         		}
              } 
-             int locationOfSpace = fileTextLine.indexOf(" ");//still handling that possible blank space in the matrix size in the file
-             String c1=fileTextLine.substring(0,locationOfSpace);
-             String r1=fileTextLine.substring(locationOfSpace+sum,fileTextLine.length());
-             column = Integer.parseInt(c1);
-             row = Integer.parseInt(r1);
-             GameMatrix=new String[row][column];//create new matrix based on the size from the file       
-          }//end if 
-          else
-             for(int i=0; i< fileTextLine.length();i++){//it is not the first line of the maze file
-                 textVar = fileTextLine.charAt(i); //grab the individual charaters from the string.
-                 if(textVar == '.') {//change . to N, so we dont have any goofy file system problems
+             int locationOfSpace = fileTextLine.indexOf(" ");
+             String c1 = fileTextLine.substring(0,locationOfSpace);
+             String r1 = fileTextLine.substring(locationOfSpace+sum,fileTextLine.length());
+             columna = Integer.parseInt(c1);
+             fila = Integer.parseInt(r1);
+             GameMatrix=new String[fila][columna];       
+          } else {
+        	  for(int i=0; i< fileTextLine.length();i++){
+                 textVar = fileTextLine.charAt(i); 
+                 if(textVar == '.') {
                 	 textVar='N';
                  }
                  String textVar1= "" + textVar;
-                 if(textVar == 'E'){//log the position of the exit for later use                  
+                 if(textVar == 'E'){                 
                      exitXCord = lineNum-1;
                      exitYCord =i;
-                     // textVar='W';
                      textVar1= "" + textVar;//turn the exit into a wall
                  }
-                 GameMatrix[lineNum-1][i]=textVar1; //load the matrix with values, aka N,W, D, H, etc
-             }//end for loop
-     }
+                 GameMatrix[lineNum-1][i] = textVar1; 
+        	  }
+          }
+	}
      
      public String[][] getGameMatrix(){ 
     	 int exitCount=0;
@@ -88,11 +87,11 @@ public class FileLoader{
      }
      
      public int getMatrixSizeColumn(){
-         return column;
+         return columna;
      }
      
      public int getMatrixSizeRow(){
-         return row;
+         return fila;
          
      }
      
